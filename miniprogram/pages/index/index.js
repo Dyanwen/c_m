3f5f0b54a152c42e8b8a324e3a37d39a9c900691
贡熙 <gongxi@wacai.com>
2021-12-30 19:22:43 +0800

    feat:update

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,7 +2,39 @@ const app = getApp() // 全局APP
 let that = null // 页面this指针
 Page({
   data: {
-    images: []
+    images: [],
+    photosArr: [
+      "https://note.youdao.com/yws/api/personal/file/WEBa0b740f731666f46391ec929303dd462?method=download&shareKey=11566dcccf27a5f1bad1ef0a422c9ab7",
+      "https://note.youdao.com/yws/api/personal/file/WEBb2116bf648acd99efc0780ae4423eaf2?method=download&shareKey=3ea96bd1d3fe65dc4a7202b24e941e8c"
+    ],
+    actors: [
+      {
+        name: "胡广豪",
+        role: "新郎",
+        avatar: "https://note.youdao.com/yws/api/personal/file/WEB279aeb99c043ce3c055e83d2858d729a?method=download&shareKey=8bd3bbdca02d39fb319002a3f8e1ca18"
+      }, {
+        name: "邓艳文",
+        role: "新娘",
+        avatar: "https://note.youdao.com/yws/api/personal/file/WEB12cfead587d219ea4c50d6f41088d414?method=download&shareKey=57b7a6fbcd3941432b7e9c466753ba00"
+      }
+    ],
+    comments: [
+      {
+        name: "易洋千玺",
+        avatar: "http://p5.itc.cn/images01/20200619/3ba30d993057465984c44517c91456b7.jpeg",
+        thumb: 7902,
+        message: "世界这么大，人生这么长，总会有个人想让你温柔对待。恭喜你们找到了彼此，往后余生，幸福相伴",
+        time: "10-20",
+        reply: 8806
+      }, {
+        name: "胡歌",
+        avatar: "https://p1.ssl.qhimg.com/dmfd/380_472_/t0126cb83ef68bc012d.jpg",
+        thumb: 7902,
+        message: "新婚快乐，愿君与另一半共挽同心结一生，共和相思曲一世。",
+        time: "11-09",
+        reply: 6606
+      }
+    ]
   },
   /**
    * 页面加载
