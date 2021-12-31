ba144a8c19ce3a0de09702d2b3defe6689241f5f
贡熙 <gongxi@wacai.com>
2021-12-31 14:24:35 +0800

    delete

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -52,7 +52,6 @@ Page({
    * 页面加载
    */
   onLoad() {
-
     // that.init() // 初始化
   },
   /**
@@ -71,56 +70,7 @@ Page({
       })
     }
   },
-  /**
-   * 更新单选框输入值
-   * @param {*} e 页面信息
-   */
-  radioChange(e) {
-    const key = `form.${e.currentTarget.dataset.key}` // 将key值带入，生成改变路径
-    that.setData({ // 更改对应路径为输入信息
-      [key]: e.detail.value
-    })
-  },
-  /**
-   * 更新输入框输入值
-   * @param {*} e 页面信息
-   */
-  oninput(e) {
-    const key = `form.${e.currentTarget.dataset.key}` // 将key值带入，生成改变路径
-    that.setData({ // 更改对应路径为输入信息
-      [key]: e.detail.value
-    })
-  },
-  /**
-   * 提交报名
-   */
-  async submit() {
-    let flag = true // 先设置flag为true，用于检查
-    const check = that.data.info.form // 取出form原始结构
-    const form = that.data.form // 取出输入的
-    for (const i in check) { // 对原始结构进行循环
-      if (form[i] == null || form[i] === '') { // 如果原始需要填写的没有写
-        wx.showModal({ // 提示要补充
-          content: `${check[i].name}未填写，请补充！`,
-          showCancel: false
-        })
-        flag = false // 设置false，跳过提交环节
-        break // 退出for循环
-      }
-    }
-    if (flag === true) { // 如果flag=true，证明验证通过
-      wx.showLoading({ // 显示加载中
-        title: '提交中',
-        mask: true
-      })
-      await app.call({ // 发起云函数，提交信息
-        name: 'add',
-        data: form
-      })
-      await that.init() // 更新信息
-      wx.hideLoading() // 隐藏加载中
-    }
-  },
+
   onShareAppMessage() {
     return {
       title: '胡广豪&邓艳文的婚礼请柬',
